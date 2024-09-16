8777eb9a79c06d6d804e6a6cb7dbe2966c16ad43
Tasuku Igarashi <igarashi.tasuku@d.mbox.nagoya-u.ac.jp>
2024-09-16 10:17:34 +0900

    unicode removed from manual

--- R/tokenize_mecab.R
+++ R/tokenize_mecab.R
@@ -7,9 +7,8 @@
 #' @param text_field A character vector. By default, the column name of \code{text} to be used.
 #' @param sys_dic A character vector. The location of the system dictionary of MeCab.
 #' @param user_dic A character vector. The location of the user dictionary of MeCab.
-#' @param original A logical value. If \code{TRUE}, the function will use the original form of words.
-#' For example, '\u601D\u3044' is transformed into the original form of '\u601D\u3046'. This is useful for
-#' counting words.
+#' @param baseform A logical value. If \code{TRUE}, the function will use the base form of words.
+#' @param original A logical value. If \code{TRUE}, Same as \code{baseform}.
 #' @param liwclike A logical value. If \code{TRUE},
 #' the function will exclude delimiters (\code{'\b'}) according to LIWC2015.
 #'
@@ -36,13 +35,13 @@
 #'
 tokenize_mecab <- function(text, text_field = "text",
                         sys_dic = getOption("jliwc_IPADIC"), user_dic = getOption("jliwc_USERDIC"),
-                        original = FALSE,
+                        baseform = FALSE, original = FALSE,
                         liwclike = TRUE) {
   text_df <- tokenize2(text, text_field = text_field, sys_dic = sys_dic, user_dic = user_dic) |>
     gibasa::prettify()
 
-  # Use original form of words
-  if (original) {
+  # Use the base form of words
+  if (any(original, baseform)) {
     text_df <- text_df |>
       # dplyr::mutate(token = ifelse(!is.na(.data[["Original"]]), .data[["Original"]], .data[["token"]]))
       dplyr::mutate(token = ifelse(!is.na(.data[["Original"]]), .data[["Original"]], .data[["token"]]))
